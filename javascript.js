09120179991cda331acfa2ee873564de98a63ccf
Greg <gregcourt10@gmail.com>
2022-11-12 16:44:54 +0000

    finalised

--- javascript.js
+++ javascript.js
@@ -1,8 +1,22 @@
+let mouseClicked = false
+document.querySelector('body').addEventListener('mousedown', () => {mouseClicked = true})
+document.querySelector('body').addEventListener('mouseup', () => {mouseClicked = false})
+
 const grid = document.getElementById('grid')
+const clearButton = document.getElementById('clear')
+const sizeSlider = document.getElementById('sizeSlider')
+let gridSize = sizeSlider.value
+let colorPicker = document.getElementById('colorPicker')
+let color = colorPicker.value
 
-let gridSize = 128
+let updateSize = () => displaySize.innerHTML = sizeSlider.value
 
-function createGrid() {
+clearButton.addEventListener('click', resetGrid)
+sizeSlider.addEventListener('input', updateSize)
+
+function createGrid(gridSize) {
+  grid.innerHTML = ''
+  gridSize = sizeSlider.value
   for (let i = 0; i < gridSize; i++) {
     let row = document.createElement("tr")
     row.id = "row" + i;
@@ -10,18 +24,37 @@ function createGrid() {
     let rowI = document.getElementById("row" + i);
 
     for(let j = 0; j < gridSize; j++) {
-      let column = document.createElement("td")
-      column.addEventListener("mouseover",draw)
-      column.id = "column" + j
-      rowI.appendChild(column)
+      let pixel = document.createElement("td")
+      pixel.addEventListener('mousedown', fillPixel)
+      pixel.addEventListener('mouseenter', fillPixel)
+      pixel.id = "pixel" + j
+      rowI.appendChild(pixel)
+      pixel.ondragstart = () => {
+        return false;
+      };
+      pixel.classList.add("bordered");
     }
   }
 }
 
-createGrid()
+createGrid(gridSize)
 
-const pixel = document.querySelectorAll('td')
+function resetGrid() {
+  createGrid(gridSize)
+}
 
-function draw() {
-  this.setAttribute('class', 'filled')
+function fillPixel() {
+  if (mouseClicked == true) {
+    this.style.backgroundColor = color
+  }
+}
+
+function changeColor(choice) {
+  color = choice
+}
+
+function toggleGrid() {
+  let pixel = document.querySelectorAll("td");
+  console.log(pixel)
+  pixel.forEach(item => item.classList.toggle('bordered'));
 }
